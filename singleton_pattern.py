7e8264a7927acf57f23e2de782c485fbcc88b230
Sajal Sharma <sajal.4591@gmail.com>
2020-05-02 13:28:15 +0530

    add singleton pattern

---
+++ singleton_pattern.py
@@ -0,0 +1,24 @@
+# https://stackoverflow.com/questions/3192095/where-exactly-the-singleton-pattern-is-used-in-real-application
+# https://dzone.com/articles/design-patterns-singleton
+# https://www.tutorialspoint.com/python_design_patterns/python_design_patterns_singleton.htm
+
+
+class Singleton():
+    __instance = None
+
+    def __init__(self):
+        if Singleton.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Singleton.__instance = self
+
+    def get_instance(self):
+        if Singleton.__instance == None:
+            Singleton.__instance = Singleton()
+        return Singleton.__instance
+
+
+s = Singleton()
+print(s)
+print(s.get_instance())
+print(s.get_instance())
